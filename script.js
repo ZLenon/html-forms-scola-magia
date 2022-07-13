d0e40dd75804eafda75078c090627ef0c844cdb4
ZLenon <lenoninformatica@gmail.com>
2022-07-13 16:46:57 -0300

    tentando

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementsByName('button');
+const email = document.getElementsByName('email');
+const senha = document.getElementsByName('password');
+
+function button(event) {
+  
+  
+}
